bb51990ec73b49a4e4e669e87452eb004552b087
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-10-30 19:56:55 +0100

    perf(compile): Precompile ES6 code
    Precompiling all ES6 code to ES5 with babel to gain performance
    Adding a .nodeignore file as well to ignore the source code and tests
    files when publishing to npm
    
    See #39

--- bin/git-cz.js
+++ bin/git-cz.js
@@ -1,5 +1,4 @@
-require("babel/register");
 var path = require('path');
-require('../src/cli/git-cz.js').bootstrap({
+require('../dist/cli/git-cz.js').bootstrap({
   cliPath: path.join(__dirname, '../')
 });
