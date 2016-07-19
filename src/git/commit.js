55c98fa8ef4b84618c5656d2b84d8ce0f40ebc00
Raphael Tomchinsky <rtomchinsky@linux.autbank>
2016-07-19 17:58:44 -0300

    fix(commit): Adds support for multilines on Windows
    Closes #286

--- src/git/commit.js
+++ src/git/commit.js
@@ -18,7 +18,7 @@ function commit(sh, repoPath, message, options, done) {
   // On windows we must use an array in gulp-git instead of a string because
   // command line parsing works differently
   if(os.platform()=="win32") {
-    operatingSystemNormalizedMessage = escapedMessage.split(/\r?\n/);
+    operatingSystemNormalizedMessage = escapedMessage.replace(/\r/g, '');
   } else {
     operatingSystemNormalizedMessage = escapedMessage.replace(/`/g, '\\`');
   }
