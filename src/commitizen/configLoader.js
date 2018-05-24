5f8902e7b8448da0e20e89bfd100c8d0287d1e6c
Matt Travi <github@travi.org>
2018-05-24 10:32:02 -0500

    fix(config): prioritized package.json last when looking for config files (#484)
    since package.json was prioritized first, it is always found and always used, even if it does not contain commitizen config. the other optional config files are never found as a result. this change looks for the optional files first and only lands on the package.json if the other files do not exist
    
    fixes #478

--- src/commitizen/configLoader.js
+++ src/commitizen/configLoader.js
@@ -3,7 +3,7 @@ import {loader} from '../configLoader';
 export { load };
 
 // Configuration sources in priority order.
-var configs = ['package.json', '.czrc', '.cz.json'];
+var configs = ['.czrc', '.cz.json', 'package.json'];
 
 function load (config, cwd) {
   return loader(configs, config, cwd);
