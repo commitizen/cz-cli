5a9674e495bcf229496200e58d7d45a0c382a664
Example User <example@example.com>
2018-09-26 22:10:22 -0500

    fix: remove path-exists in favor of fs.existsSync

--- src/configLoader/getContent.js
+++ src/configLoader/getContent.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { sync as existsSync } from 'path-exists';
+
 import stripJSONComments from 'strip-json-comments';
 
 import { getNormalizedConfig } from '../configLoader';
@@ -54,7 +54,7 @@ function getConfigContent (configPath, baseDirectory) {
     const resolvedPath = path.resolve(baseDirectory, configPath);
     const configBasename = path.basename(resolvedPath);
 
-    if (!existsSync(resolvedPath)) {
+    if (!fs.existsSync(resolvedPath)) {
       return getNormalizedConfig(resolvedPath);
     }
 
