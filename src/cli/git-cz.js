6e265af15645799340c4ff92e65b4975952f809b
Felipe Santos <felipecassiors@gmail.com>
2020-09-14 14:49:47 +0000

    feat!: use cz-conventional-changelog as default adapter

--- src/cli/git-cz.js
+++ src/cli/git-cz.js
@@ -1,5 +1,5 @@
 import { configLoader } from '../commitizen';
-import { git as useGitStrategy, gitCz as useGitCzStrategy } from './strategies';
+import { gitCz as useGitCzStrategy } from './strategies';
 
 export {
   bootstrap
@@ -9,19 +9,12 @@ export {
  * This is the main cli entry point.
  * environment may be used for debugging.
  */
-function bootstrap (environment = {}, argv = process.argv) {
+function bootstrap(environment = {}, argv = process.argv) {
 
   // Get cli args
   let rawGitArgs = argv.slice(2, argv.length);
 
-  let adapterConfig = environment.config || configLoader.load();
+  let adapterConfig = environment.config || configLoader.load() || { path: 'cz-conventional-changelog' };
 
-  // Choose a strategy based on the existance the adapter config
-  if (typeof adapterConfig !== 'undefined') {
-    // This tells commitizen we're in business
-    useGitCzStrategy(rawGitArgs, environment, adapterConfig);
-  } else {
-    // This tells commitizen that it is not needed, just use git
-    useGitStrategy(rawGitArgs, environment);
-  }
+  useGitCzStrategy(rawGitArgs, environment, adapterConfig);
 }
