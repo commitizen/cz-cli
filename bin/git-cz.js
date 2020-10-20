95a20d45d4af05eace9693a346e45d8bc9eabc2c
hdmr14 <58992133+hdmr14@users.noreply.github.com>
2020-10-21 03:48:17 +0900

    fix(cli): Exit CLI with 1(as failed) when received SIGINT (#736)
    * fix: Exit process as failed when catch SIGINT.
    
    * Exit on SIGINT with correct POSIX value
    
    Co-authored-by: David Welch <dmwelch@users.noreply.github.com>

--- bin/git-cz.js
+++ bin/git-cz.js
@@ -3,7 +3,14 @@ var path = require('path');
 process.on('uncaughtException', function (err) {
   console.error(err.message || err);
   process.exit(1);
-})
+});
+
+// catch SIGINT signal
+process.stdin.on('data', function (key) {
+  if (key == '\u0003') {
+    process.exit(130); // 128 + SIGINT
+  }
+});
 
 require('../dist/cli/git-cz.js').bootstrap({
   cliPath: path.join(__dirname, '../')
