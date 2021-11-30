d8eb0d4d53f62d42d68eaa97d0176991be42e5e5
Dave Welch <dmwelch@users.noreply.github.com>
2021-11-30 10:23:11 -0600

    refactor(logos): dynamically generate pngs from svg files
    re #800

---
+++ logo/generate.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+#####################################################
+# Script to create PNG files from SVG source files. #
+# Dependencies: cairosvg (https://cairosvg.org/)    #
+#                                                   #
+# Rationale: Generating from SVG instead of storing #
+#   image files decreases size of repo and avoids   #
+#   security vulnerabilites associated with binary  #
+#   image files.                                    #
+#####################################################
+
+convert () {
+  COLOR=$1
+  # echo "converting file $COLOR..."
+  FILE=logo/commitizen-logo-${COLOR}.svg
+  if [ ! -f ${FILE} ]; then
+    # echo "file $FILE missing!"
+    exit 1
+  fi
+  echo "conversion in progress..."
+  for SIZE in 16 48 96 256 512 1024; do
+    cairosvg ${FILE} -f png -W ${SIZE} -H ${SIZE} -d 300 -o logo/commitizen_logo_${COLOR}_${SIZE}x${SIZE}.png
+  done
+  # echo "done"
+  return 0
+}
+
+if [ ! -e $(which cairosvg) ]; then
+  pip3 install cairosvg
+fi
+
+for COLOR in black blue white color; do
+  FILE=logo/commitizen-logo-${COLOR}.svg
+  if [ ! -f ${FILE} ]; then
+    # echo "generating file $FILE..."
+    sed 's|fill=\".*\"|fill=\"'${COLOR}'\"|g' <logo/commitizen-logo-mono.svg >${FILE}
+    convert ${COLOR}
+    # echo "removing generated file $FILE..."
+    rm $FILE
+    # echo "success!"
+  else
+    # echo "file found: $FILE"
+    convert ${COLOR}
+  fi
+done
+
+exit 0
